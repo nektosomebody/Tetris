cebdd5d8c8128064cc4f3e2437c2e1c726d96572
ulyana <shirikovaulyanochka@yandex.ru>
2023-02-22 17:14:22 +0300

    last commit

--- ghd.py
+++ ghd.py
@@ -21,29 +21,6 @@ def load_image(name, color_key=None):
     return image
 
 
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
-        super().__init__()
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
-
-
 class Board:
     def __init__(self):
         self.board = [['.'] * COUNT_W for _ in range(COUNT_H)]
@@ -59,7 +36,7 @@ class Board:
                 for i in range(len(figure.positions)):
                     y, x = figure.positions[i]
                     if BOARD.board[y][x] == '*':
-                        screen.blit(image, (x * 30 + 10, y * 30 + 10))
+                        screen.blit(image, (x * 30 + BORDER_LEFT_RIGHT, y * 30 + BORDER_UP))
 
     def change_y(self, del_y):
         for j in range(len(self.figures)):
@@ -105,8 +82,8 @@ class Figure(pygame.sprite.Sprite):
         can_move_l, can_move_r = True, True
         for i in range(self.count_cubes):
             y, x = self.positions[i]
-            if x - 1 < 0 or BOARD.board[y][x - 1] != '.' and (y, x - 1) not in self.positions:  # проверяем что слева свободно...
-                # ... и что мы не перемещаемся в себя
+            if x - 1 < 0 or BOARD.board[y][x - 1] != '.' and (y, x - 1) not in self.positions:
+                # проверяем что слева свободно и что мы не перемещаемся в себя
                 can_move_l = False
             if x + 1 >= COUNT_W or BOARD.board[y][x + 1] != '.' and (y, x + 1) not in self.positions:
                 can_move_r = False
@@ -171,6 +148,7 @@ class Figure(pygame.sprite.Sprite):
 def check_on_line():
     one = ['*'] * COUNT_W
     y_to_clean = []
+    """sp_under_red_line = False"""
     for i in range(COUNT_H):
         if BOARD.board[i] == one:
             y_to_clean.append(i)
@@ -179,6 +157,7 @@ def check_on_line():
 
 def cleaning(clean_y):
     global BOARD
+    global TOTAL_SCORE
     list_of_image_in_line = []  # список линий где нужно удалить квадраты
     for y in clean_y:
         one_line = []
@@ -204,7 +183,9 @@ def cleaning(clean_y):
         BOARD.board.insert(0, ['.'] * COUNT_W)
         BOARD.board_index.insert(0, [None] * COUNT_W)
         BOARD.change_y(y)
+        TOTAL_SCORE += COUNT_OF_POINTS * COUNT_W
         screen.fill((255, 255, 255))
+        design()
         BOARD.draw()
         pygame.display.flip()
         clock.tick(FPS)
@@ -222,12 +203,69 @@ def make_small(image):
     return ret_lst
 
 
+def game_over():
+    screen.fill(pygame.color.Color('white'))
+
+    font = pygame.font.Font(None, 50)  # score
+    score = font.render(f"TOTAL SCORE {TOTAL_SCORE}", 1, pygame.color.Color('black'))
+    score_rect = score.get_rect()
+    screen.blit(score,
+                ((COUNT_W * one_sq - score_rect.width) // 2, (COUNT_H * one_sq - score_rect.height) // 3))
+
+    # button 1
+    pygame.draw.rect(screen, pygame.color.Color('black'), (100, 100, 50, 50), border_radius=1)
+
+    # button 2
+    pygame.draw.rect(screen, pygame.color.Color('black'), (200, 100, 50, 50), border_radius=1)
+    """run = True
+    img = load_image('gameover.png')
+    img_rect = img.get_rect()
+    img_rect.x = -600
+    dist = 20
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        if img_rect.x > -20:
+            dist = 0
+        img_rect.x += dist
+        screen.fill((255, 255, 255))
+        screen.blit(img, img_rect)
+        pygame.display.update()
+        clock.tick(FPS)"""
+
+
+def design():
+    pygame.draw.rect(screen, (115, 210, 70), (0, BORDER_UP + COUNT_H * one_sq,
+                                              COUNT_W * one_sq + BORDER_LEFT_RIGHT * 2, BORDER_DOWN))
+
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, COUNT_W * one_sq + BORDER_LEFT_RIGHT * 2, BORDER_UP))      # top
+    pygame.draw.rect(screen, (0, 0, 0), (0, 0, BORDER_LEFT_RIGHT, one_sq * COUNT_H + BORDER_UP))      # left
+    pygame.draw.rect(screen, (0, 0, 0), (BORDER_LEFT_RIGHT + COUNT_W * one_sq, 0, BORDER_LEFT_RIGHT,
+                                         BORDER_UP + one_sq * COUNT_H))       # right
+
+    pygame.draw.rect(screen, (255, 0, 0), (BORDER_LEFT_RIGHT, BORDER_UP + one_sq * 3, COUNT_W * one_sq, 1))   # red line
+
+    font = pygame.font.Font(None, 24)   # score
+    score = font.render(f"YOUR SCORE {TOTAL_SCORE}", 1, pygame.color.Color('black'))
+    score_rect = score.get_rect()
+    screen.blit(score, ((COUNT_W * one_sq - score_rect.width) // 2, COUNT_H * one_sq + (BORDER_DOWN - score_rect.height) // 2))
+
+
 if __name__ == '__main__':
     pygame.init()
+    pygame.display.set_caption('TETRIS')
+
     FPS = 10
-    COUNT_W, COUNT_H = 24, 30
+    COUNT_W, COUNT_H = 20, 20
+    BORDER_DOWN = 50
+    BORDER_UP = 10
+    BORDER_LEFT_RIGHT = 10
     one_sq = 30
-    size = 20 + COUNT_W * one_sq, 20 + COUNT_H * one_sq
+    TOTAL_SCORE = 0
+    COUNT_OF_POINTS = 5
+
+    size = BORDER_LEFT_RIGHT * 2 + COUNT_W * one_sq, 10 + BORDER_DOWN + COUNT_H * one_sq
     screen = pygame.display.set_mode(size)
     clock = pygame.time.Clock()
     running = True
@@ -237,22 +275,23 @@ if __name__ == '__main__':
     SCELETONS = [[2, 3, (0, 0), (0, 0 - 1), (1, 0 - 1), (2, 0 - 1)],  # width, height, points, (delta_y, delta_x) ...
                  [3, 2, (0, 0), (1, 0), (1, 0 + 1), (1, 0 + 2)],
                  [4, 1, (0, 0), (0, 1), (0, 2), (0, 3)],
-                 [2, 2, (0, 0), (0, 1), (1, 0), (1, 1)]]
-    ALL_COLORS = ['red.png', 'yellow.png', 'green.png']
-
-    ANIMATED_SPRITES = [AnimatedSprite(load_image("cut_sheet_green.png", (0, 255, 255)), 3, 1, 50, 50),
-                        AnimatedSprite(load_image("cut_sheet_red.png", (0, 255, 255)), 3, 1, 50, 50),
-                        AnimatedSprite(load_image("cut_sheet_yellow.png", (0, 255, 255)), 3, 1, 50, 50)]
+                 [2, 2, (0, 0), (0, 1), (1, 0), (1, 1)],
+                 [3, 2, (0, 0), (1, -1), (1, 0), (1, 1)],
+                 [3, 2, (0, 0), (0, 1), (0, 2), (1, 1)],
+                 [1, 4, (0, 0), (1, 0), (2, 0), (3, 0)],
+                 [2, 1, (0, 0), (0, 1)]]
+    ALL_COLORS = ['red.png', 'yellow.png', 'green.png', 'blue.png', 'pink.png']
 
     BOARD = Board()
     active_figure = None
 
     while running:
         screen.fill(pygame.Color('white'))
+        design()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN: # active_figure is None:
                 active_figure = Figure(random.choice(ALL_COLORS))
             if event.type == pygame.KEYDOWN and active_figure is not None:
                 if event.key == pygame.K_LEFT:
@@ -267,6 +306,9 @@ if __name__ == '__main__':
         clean_y = check_on_line()
         if clean_y and not active_figure.checking_down():
             cleaning(clean_y)
+        if '*' in BOARD.board[2] and active_figure is None:
+            pass
+            # game_over()
         clock.tick(FPS)
         pygame.display.flip()
     pygame.quit()
